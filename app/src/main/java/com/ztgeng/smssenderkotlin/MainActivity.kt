0c1c76c4e4e4f7e4d8859025414519d505c5f67b
ztgeng <zhtgeng@gmail.com>
2019-01-06 18:13:25 -0800

    Change to JsonArrayRequest

--- app/src/main/java/com/ztgeng/smssenderkotlin/MainActivity.kt
+++ app/src/main/java/com/ztgeng/smssenderkotlin/MainActivity.kt
@@ -9,10 +9,14 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.AppCompatButton
 import android.util.Log
+import com.android.volley.NetworkResponse
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+import com.android.volley.toolbox.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.UnsupportedEncodingException
+import java.nio.charset.Charset
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +29,8 @@ class MainActivity : AppCompatActivity() {
 
         val button: AppCompatButton = findViewById(R.id.button)
         button.setOnClickListener {
-//            Log.d("gengz", readSms(5).joinToString())
-            sendPost("http://192.168.0.25:8000", readSms(8).joinToString())
+//            Log.d("gengz", readSms(5))
+            sendPost("http://192.168.0.25:8000", readSms(3))
         }
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
@@ -35,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             Log.d("gengz", "Already granted permission")
         }
 
-//        Log.d("gengz", readSms(5).joinToString())
+//        Log.d("gengz", readSms(5))
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -50,43 +54,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun readSms(number: Int): List<String> {
-        val msgs = mutableListOf<String>()
+    private fun readSms(number: Int): JSONArray {
+        val msgs = JSONArray()
         val cursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null) ?: return msgs
         if (cursor.moveToFirst()) {
             do {
-                var msg = "{\n"
+                val msg = JSONObject()
                 for (i in 0 until cursor.columnCount) {
-                    msg += when (cursor.getColumnName(i)) {
-                        "address" -> "  \"number\": \" ${cursor.getString(i)}\",\n"
-                        "date" -> "  \"time\": \" ${Date(cursor.getLong(i))}\",\n"
-                        "body" -> "  \"message\": \" ${cursor.getString(i)}\"\n"
-                        else -> ""
+                    when (cursor.getColumnName(i)) {
+                        "address" -> msg.put("number", cursor.getString(i))
+                        "date" -> msg.put("time", Date(cursor.getLong(i)).toString())
+                        "body" -> msg.put("message", cursor.getString(i))
                     }
                 }
-                msg += "}"
-                msgs.add(msg)
-            } while (cursor.moveToNext() && msgs.size < number)
+                msgs.put(msg)
+            } while (cursor.moveToNext() && msgs.length() < number)
         }
         cursor.close()
         return msgs
     }
 
-    private fun sendPost(url: String, json: String) {
+    private fun sendPost(url: String, jsonArray: JSONArray) {
         val queue = Volley.newRequestQueue(this)
-        // Request a string response from the provided URL.
-        val stringRequest = object : StringRequest(
+        val request = object : JsonRequest<String>(
                 Request.Method.POST,
                 url,
-                Response.Listener<String> { response ->
-                    Log.d("gengz", response)
-                },
-                Response.ErrorListener { Log.d("gengz", "error!") }) {
-            override fun getBodyContentType() = "application/json; charset=utf-8"
-            override fun getBody() = json.toByteArray()
-        }
+                jsonArray.toString(),
+                Response.Listener<String> { response -> Log.d("gengz", response.toString()) },
+                Response.ErrorListener { error -> Log.d("gengz", error.message) }) {
+            override fun parseNetworkResponse(response: NetworkResponse): Response<String> {
+                val parsed: String = try {
+                    String(response.data, Charset.forName(HttpHeaderParser.parseCharset(response.headers)))
+                } catch (e: UnsupportedEncodingException) {
+                    String(response.data, Charset.defaultCharset())
+                }
 
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest)
+                return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response))
+            }
+        }
+        queue.add(request)
     }
 }
