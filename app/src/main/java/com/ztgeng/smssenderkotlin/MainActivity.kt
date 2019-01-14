107c0948afed0ecd525863150495f89511130bf8
ztgeng <zhtgeng@gmail.com>
2019-01-14 03:08:03 -0800

    Input server. NetworkClient. FCM service

--- app/src/main/java/com/ztgeng/smssenderkotlin/MainActivity.kt
+++ app/src/main/java/com/ztgeng/smssenderkotlin/MainActivity.kt
@@ -1,23 +1,20 @@
 package com.ztgeng.smssenderkotlin
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.AppCompatButton
+import android.support.v7.widget.AppCompatEditText
 import android.util.Log
-import com.android.volley.NetworkResponse
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.*
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.UnsupportedEncodingException
-import java.nio.charset.Charset
-import java.util.*
+import android.widget.Toast
+import com.google.firebase.iid.FirebaseInstanceId
+
+const val PREFS_NAME = "SmsSender"
+const val SERVER_NAME = "server"
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,19 +24,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val editText: AppCompatEditText = findViewById(R.id.server_input)
+        editText.setText(getSharedPreferences(PREFS_NAME, MODE_PRIVATE).getString(SERVER_NAME, ""))
+
         val button: AppCompatButton = findViewById(R.id.button)
         button.setOnClickListener {
-//            Log.d("gengz", readSms(5))
-            sendPost("http://192.168.0.25:8000", readSms(3))
+            val url = editText.text?.toString()
+            if (!url.isNullOrBlank()) {
+                getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).edit().putString(SERVER_NAME, url).apply()
+                getFCMToken()
+            } else {
+                Toast.makeText(this, "Invalid url", Toast.LENGTH_SHORT).show()
+            }
         }
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS), permissionRequestCode)
-        } else {
-            Log.d("gengz", "Already granted permission")
         }
-
-//        Log.d("gengz", readSms(5))
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -54,44 +55,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun readSms(number: Int): JSONArray {
-        val msgs = JSONArray()
-        val cursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null) ?: return msgs
-        if (cursor.moveToFirst()) {
-            do {
-                val msg = JSONObject()
-                for (i in 0 until cursor.columnCount) {
-                    when (cursor.getColumnName(i)) {
-                        "address" -> msg.put("number", cursor.getString(i))
-                        "date" -> msg.put("time", Date(cursor.getLong(i)).toString())
-                        "body" -> msg.put("message", cursor.getString(i))
-                    }
+    private fun getFCMToken() {
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener { task ->
+            run {
+                if (!task.isSuccessful) {
+                    Log.d("gengz", "Fail to get FCM token!")
+                    return@run
                 }
-                msgs.put(msg)
-            } while (cursor.moveToNext() && msgs.length() < number)
-        }
-        cursor.close()
-        return msgs
-    }
-
-    private fun sendPost(url: String, jsonArray: JSONArray) {
-        val queue = Volley.newRequestQueue(this)
-        val request = object : JsonRequest<String>(
-                Request.Method.POST,
-                url,
-                jsonArray.toString(),
-                Response.Listener<String> { response -> Log.d("gengz", response.toString()) },
-                Response.ErrorListener { error -> Log.d("gengz", error.message) }) {
-            override fun parseNetworkResponse(response: NetworkResponse): Response<String> {
-                val parsed: String = try {
-                    String(response.data, Charset.forName(HttpHeaderParser.parseCharset(response.headers)))
-                } catch (e: UnsupportedEncodingException) {
-                    String(response.data, Charset.defaultCharset())
-                }
-
-                return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response))
+                val token = task.result?.token
+                Log.d("gengz", "Get FCM token: $token")
+                NetworkClient.getInstance(this.applicationContext).sendToken(this, token ?: return@run)
             }
         }
-        queue.add(request)
     }
 }
---
+++ app/src/main/java/com/ztgeng/smssenderkotlin/NetworkClient.kt
@@ -0,0 +1,76 @@
+package com.ztgeng.smssenderkotlin
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.NetworkResponse
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.HttpHeaderParser
+import com.android.volley.toolbox.JsonRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import java.io.UnsupportedEncodingException
+import java.nio.charset.Charset
+import kotlin.jvm.Volatile
+
+class NetworkClient(context: Context){
+    companion object {
+        @Volatile
+        private var INSTANCE: NetworkClient? = null
+        fun getInstance(context: Context) = INSTANCE ?: synchronized(this) {
+            INSTANCE ?: NetworkClient(context).also {
+                INSTANCE = it
+            }
+        }
+    }
+
+    private val requestQueue: RequestQueue by lazy {
+        // applicationContext is key, it keeps you from leaking the
+        // Activity or BroadcastReceiver if someone passes one in.
+        Volley.newRequestQueue(context.applicationContext)
+    }
+
+    private fun getUrl(context: Context) = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).getString(SERVER_NAME, null)
+
+    fun sendToken(context: Context, token: String) {
+        sendString(getUrl(context) ?: return, token)
+    }
+
+    fun sendSms(context: Context, data: JSONArray) {
+        sendJsonArray(getUrl(context) ?: return, data)
+    }
+
+    private fun sendJsonArray(url: String, jsonArray: JSONArray) {
+        val request = object : JsonRequest<String>(
+                Request.Method.POST,
+                "http://$url/sms",
+                jsonArray.toString(),
+                Response.Listener<String> { response -> Log.d("gengz", response.toString()) },
+                Response.ErrorListener { error -> Log.d("gengz", error.toString()) }) {
+            override fun parseNetworkResponse(response: NetworkResponse): Response<String> {
+                val parsed: String = try {
+                    String(response.data, Charset.forName(HttpHeaderParser.parseCharset(response.headers)))
+                } catch (e: UnsupportedEncodingException) {
+                    String(response.data, Charset.defaultCharset())
+                }
+
+                return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response))
+            }
+        }
+        requestQueue.add(request)
+    }
+
+    private fun sendString(url: String, data: String) {
+        val request = object : StringRequest(
+                Request.Method.POST,
+                "http://$url/token",
+                Response.Listener<String> { response -> Log.d("gengz", response.toString()) },
+                Response.ErrorListener { error -> Log.d("gengz", error.toString()) }) {
+            override fun getBodyContentType() = "text/plain; charset=utf-8"
+            override fun getBody() = data.toByteArray()
+        }
+        requestQueue.add(request)
+    }
+}
